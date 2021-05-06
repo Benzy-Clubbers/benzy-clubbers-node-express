4fa6889434b2a9fdc07d701ed9b77b77908081be
Max <maxbogat717@gmail.com>
2021-05-06 12:22:56 -0700

    Created POST request for Reviews

--- routes/car.routes.js
+++ routes/car.routes.js
@@ -5,13 +5,18 @@ const router = express.Router();
 const modelController = require("../controllers/car_model.controller");
 const trimController = require("../controllers/car_trim.controller");
 const imageController = require("../controllers/car_image.controller");
+const reviewController = require("../controllers/car_review.controller");
 
 router.get("/models", modelController.listAllModels);
 
-router.get("/models/:id", modelController.findModelById);
+router.get("/model/:id", modelController.findModelById);
 
-router.get("/models/:id/trims", trimController.listTrimsById);
+router.get("/model/:id/trims", trimController.listTrimsById);
 
-router.get("/models/:id/images", imageController.listImagesById);
+router.get("/model/:id/images", imageController.listImagesById);
+
+router.get("/model/:id/reviews", reviewController.findReviewsById);
+
+router.post("/model/:id", reviewController.postReview);
 
 module.exports = router;
